4a04a0a32fed733329b29dc0acebd08d81a9e024
hudakl <laszlo.hudak@liferay.com>
2020-03-31 16:51:43 +0200

    LPS-106586 Escape the properties to be exported

--- modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/portlet/action/ExportConfigurationMVCResourceCommand.java
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/portlet/action/ExportConfigurationMVCResourceCommand.java
@@ -27,6 +27,9 @@ import com.liferay.portal.configuration.metatype.definitions.ExtendedAttributeDe
 import com.liferay.portal.configuration.metatype.definitions.ExtendedObjectClassDefinition;
 import com.liferay.portal.kernel.portlet.PortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.resource.manager.ClassLoaderResourceManager;
+import com.liferay.portal.kernel.settings.LocationVariableResolver;
+import com.liferay.portal.kernel.settings.SettingsLocatorHelper;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -304,8 +307,16 @@ public class ExportConfigurationMVCResourceCommand
 				continue;
 			}
 
-			Object value = AttributeDefinitionUtil.getPropertyObject(
-				attributeDefinition, configuration);
+			LocationVariableResolver locationVariableResolver =
+				new LocationVariableResolver(
+					new ClassLoaderResourceManager(
+						configurationModel.getClassLoader()),
+					_settingsLocatorHelper);
+
+			Object value = ConfigurationExporter.escapeProperties(
+				AttributeDefinitionUtil.getPropertyObject(
+					attributeDefinition, configuration),
+				locationVariableResolver);
 
 			if (value == null) {
 				continue;
@@ -319,5 +330,8 @@ public class ExportConfigurationMVCResourceCommand
 
 	@Reference
 	private ConfigurationModelRetriever _configurationModelRetriever;
+
+	@Reference
+	private SettingsLocatorHelper _settingsLocatorHelper;
 
 }
