d27fdfad1b5a7c1860935dfc0420d1ffa65836ca
Eric Yan <eric.yan@liferay.com>
2023-05-25 11:43:50 -0700

    LPS-157425 Apply field to EntityModel classes in headless-delivery-impl

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/odata/entity/v1_0/BlogPostingImageEntityModel.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/odata/entity/v1_0/BlogPostingImageEntityModel.java
@@ -16,6 +16,7 @@ package com.liferay.headless.delivery.internal.odata.entity.v1_0;
 
 import com.liferay.headless.common.spi.odata.entity.EntityFieldsMapFactory;
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -51,8 +52,10 @@ public class BlogPostingImageEntityModel implements EntityModel {
 			new StringEntityField(
 				"title",
 				locale -> Field.getSortableFieldName(
-					"localized_title_".concat(
-						LocaleUtil.toLanguageId(locale)))));
+					"localized_title_".concat(LocaleUtil.toLanguageId(locale))),
+				locale -> StringBundler.concat(
+					"localized_title_", LocaleUtil.toLanguageId(locale),
+					".keyword")));
 	}
 
 	@Override
