6d1681e97ad4e7bbd38d4d8a2a2c8e4b492dd6ed
Eric Yan <eric.yan@liferay.com>
2023-11-13 15:50:08 -0500

    LPS-201574 Rely on servlet request to provide the proper host name, to better support HTTP/2 (h2)
    Based on the Servlet Request API documentation, the servlet request
    first tries to use the Host header field, which would be similar to
    our current logic.
    
    With the newer HTTP/2 protocol, the Host header field is no longer
    used and has instead been replaced by the pseudo-header:
    ":authority".
    
    On Tomcat, httpServletRequest.getServerName() accounts for both
    the Host header field and the pseudo-header: ":authority" field,
    and returns the proper Host name when using HTTP/1.1 or HTTP/2.

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -2362,27 +2362,7 @@ public class PortalImpl implements Portal {
 	public String getHost(HttpServletRequest httpServletRequest) {
 		httpServletRequest = getOriginalServletRequest(httpServletRequest);
 
-		String host = httpServletRequest.getHeader("Host");
-
-		if (host != null) {
-			host = StringUtil.toLowerCase(host.trim());
-
-			// See RFC-3986 (section 3.2.2).
-
-			int pos = host.indexOf(']');
-
-			if ((pos > 0) && host.startsWith("[")) {
-				return host.substring(1, pos);
-			}
-
-			pos = host.indexOf(':');
-
-			if (pos >= 0) {
-				host = host.substring(0, pos);
-			}
-		}
-
-		return host;
+		return httpServletRequest.getServerName();
 	}
 
 	@Override
