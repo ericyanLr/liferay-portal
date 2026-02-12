4bf7b8c9ba081e7064ce0bc762251d278ba51228
Eric Yan <eric.yan@liferay.com>
2026-02-12 11:26:43 -0800

    LPD-77695 Process auto deploy files before starting the Module Framework
    Using Liferay in a cloud environment typically involves creating
    an immutable custom Liferay container image, with customizations
    being placed in the deploy folder. This means that files added to
    the deploy folder would need to be processed when a new container
    is started.
    
    By processing the deploy folder earlier, this ensures that the
    customizations are moved to their respective 'osgi/' directories
    before the module framework is started.
    
    This provides a more consistent start up by avoiding the scenario
    of custom modules getting deployed during module framework start up.

--- portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
+++ portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.concurrent.SystemExecutorServiceUtil;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.deploy.auto.AutoDeployDir;
 import com.liferay.portal.kernel.deploy.hot.HotDeployUtil;
 import com.liferay.portal.kernel.exception.LoggedExceptionInInitializerError;
 import com.liferay.portal.kernel.log.Log;
@@ -353,6 +354,12 @@ public class PortalContextLoaderListener extends ContextLoaderListener {
 				_log.error("Unable to create " + tempDirPath, ioException);
 			}
 		}
+
+		AutoDeployDir autoDeployDir = new AutoDeployDir(
+			AutoDeployDir.DEFAULT_NAME,
+			new File(PropsValues.AUTO_DEPLOY_DEPLOY_DIR), 0);
+
+		autoDeployDir.scanDirectory();
 
 		ModuleFrameworkUtil.createFramework();
 
