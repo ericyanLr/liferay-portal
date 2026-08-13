f062f1090a0151aec993377013cf9d679c7a7bb4
Eric Yan <eric.yan@liferay.com>
2026-08-13 10:33:36 -0700

    LPD-99715 SF

--- portal-impl/src/com/liferay/portal/spring/hibernate/PortalHibernateConfiguration.java
+++ portal-impl/src/com/liferay/portal/spring/hibernate/PortalHibernateConfiguration.java
@@ -128,7 +128,7 @@ public class PortalHibernateConfiguration
 		Configuration configuration = new Configuration(
 			new MetadataSources(bootstrapServiceRegistryBuilder.build()));
 
-		SQLTransformer.populateSqlFunctions(configuration);
+		SQLTransformer.populateSQLFunctions(configuration);
 
 		if (_mvccEnabled) {
 			configuration.setStatementInspector(new CTSQLInterceptor());
